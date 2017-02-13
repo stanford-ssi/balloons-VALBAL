8af2ca7c90f908e729af6dd284365b7c5502e901
Davy Ragland <dragland@stanford.edu>
2017-02-12 16:35:21 -0800

    Updated cutdown.

---
+++ src/Cutdown.h
@@ -0,0 +1,17 @@
+/*
+  Stanford Student Space Initiative
+  Balloons | VALBAL | February 2017
+  Davy Ragland | dragland@stanford.edu
+
+  File: Cutdown.h
+  --------------------------
+  Secret cutdown command.
+*/
+
+#ifndef CUTDOWN_H
+#define CUTDOWN_H
+
+/****************************  EDITABLE CONSTANTS  ****************************/
+static const char CUTDOWN_COMAND[]   = "NOT REAL STRING";
+
+#endif

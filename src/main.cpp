55dae7154f0e5efd8d5c41d733531ce4ae53979e
Davy Ragland <dragland@stanford.edu>
2017-06-05 17:39:40 -0700

    Updated TODO.

--- src/main.cpp
+++ src/main.cpp
@@ -45,8 +45,15 @@ bool ISBDCallback() {
 }
 
 /* TODO
-1) Rockblock sleep must be implemented properly.
-2) A sleep counter should be sent over RB.
-3) A way to disable sleep should be implemented.
-4) Restart should be implemented in case all else fails
+1) remove isbd.begin() from setup, but keep the diags and console setup, and the hw serial begin for rb
+2) at the start of your transmission, do isbd.begin()
+3) remember that it returns an int and lets you know if it was successful, log that
+4) transmit and log the success if transmit as well
+5) isbd.sleep() and the end, and log the success of that as well, I used to get ISBD_PROTOCOL_ERROR when I did sleep before…
+6) if isbd.begin() fails or sibd.sleep(), you want to restart the system by fetting it off for 2min and then back on
+7) if transmit fails but begin() and sleep() are successful, do nothing
+8) if you call sleep and you are already asleep, it will let you know that, in that case, something has gone terribly wrong and you need to reset
+9) basically anything other than a pure success should result in a full hard power reset
+10) A sleep counter should be sent over RB.
+11) A way to disable sleep should be implemented.
 */

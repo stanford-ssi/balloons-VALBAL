947096f3008def48c216e1520b6426361338368d
Davy Ragland <dragland@stanford.edu>
2017-07-21 01:15:31 -0700

    Created files for AD5246 library.

---
+++ lib/AD5246/AD5246.cpp
@@ -0,0 +1,46 @@
+/*
+  Stanford Student Space Initiative
+  Balloons | VALBAL | July 2017
+  Vaiva Snapkauskaite | vaivas@stanford.edu
+  Davy Ragland        | dragland@stanford.edu
+
+  File: AD5246.cpp
+  --------------------------
+  Implimentation of AD5246.h
+*/
+
+#include "AD5246.h"
+
+/**********************************  SETUP  ***********************************/
+/*
+ * Function: init
+ * -------------------
+ * This function initializes the AD5246 resistor.
+ */
+bool AD5246::init() {
+  bool success = false;
+  return success;
+}
+
+/********************************  FUNCTIONS  *********************************/
+/*
+ * Function: setResistance
+ * -------------------
+ * This function sets the resistor to the desired resistance
+ */
+bool AD5246::setResistance(float ohms) {
+  bool success = false;
+  uint8_t step = ohmsToSteps(ohms);
+  return success;
+}
+
+/*********************************  HELPERS  **********************************/
+/*
+ * Function: ohmsToSteps
+ * -------------------
+ * This function converts an arbritary resistance to the discrite
+ * step needed for the 128 step resistor.
+ */
+uint8_t AD5246::ohmsToSteps(float ohms) {
+  return 0;
+}
---
+++ lib/AD5246/AD5246.h
@@ -0,0 +1,30 @@
+/*
+  Stanford Student Space Initiative
+  Balloons | VALBAL | July 2017
+  Vaiva Snapkauskaite | vaivas@stanford.edu
+  Davy Ragland        | dragland@stanford.edu
+
+  File: AD5246.h
+  --------------------------
+  Interface to 100kOhm i2c AD5246 resistor
+*/
+
+#ifndef AD5246_H
+#define AD5246_H
+
+#include <Arduino.h>
+
+class AD5246 {
+public:
+/**********************************  SETUP  ***********************************/
+  bool    init();
+/********************************  FUNCTIONS  *********************************/
+  bool    setResistance(float ohms);
+private:
+/*********************************  HELPERS  **********************************/
+  uint8_t ohmsToSteps(float ohms);
+/*********************************  OBJECTS  **********************************/
+  static const uint16_t ADDRESS;
+};
+
+#endif

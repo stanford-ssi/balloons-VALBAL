756041da779043acc0d8961cda736d05d9276ecc
Jonathan Zwiebel <jonathanzwiebel@gmail.com>
2018-04-30 18:02:39 -0700

    Added CSV logging to SHITL down

--- sim/serial/vbserialshitl.py
+++ sim/serial/vbserialshitl.py
@@ -5,9 +5,17 @@ import pandas as pd
 
 BAUD = 9600
 
-assert len(sys.argv) == 3
+assert len(sys.argv) >= 3
+output = False
+if len(sys.argv) == 4:
+	output = True
 PORT = sys.argv[1]
 DATA_PATH = sys.argv[2]
+if output:
+	OUTPUT_PATH = sys.argv[3]
+	search_list = [b"numExecNow:", b"Altitude:", b"RAW_PRESSURE_1:", b"RAW_PRESSURE_2:", b"RAW_PRESSURE_3:", b"RAW_PRESSURE_4:"]
+	output_index = 0
+
 START_SIGNAL = b'SERIAL_SHITL_BEGIN'
 REQUEST_SIGNAL = b'SERIAL_SHITL_REQUEST'
 
@@ -23,7 +31,6 @@ while(False):
 	print(incoming)
 	if incoming == START_SIGNAL:
 		print('\n>>> ValBal ready for SHITL testing')
-		# TODO: Have AV wait for command singal form simulation
 		break
 
 while(True):
@@ -42,6 +49,15 @@ while(True):
 		vb.write(struct.pack('f', float(list(data_row['raw_pressure_2'])[0])))
 		vb.write(struct.pack('f', float(list(data_row['raw_pressure_3'])[0])))
 		vb.write(struct.pack('f', float(list(data_row['raw_pressure_4'])[0])))		
+	elif output and len(incoming.split()) == 2 and incoming.split()[0] in search_list:
+		if incoming.split()[0] == search_list[output_index]:
+			with open(OUTPUT_PATH, "a") as d_out:
+				d_out.write(str(float(incoming.split()[1])) + ",")
+			output_index += 1
+			if output_index == len(search_list):
+				with open(OUTPUT_PATH, "a") as d_out:
+					d_out.write("\n")
+				output_index = 0
+		print(incoming)
 	else: 
 		print(incoming)
-

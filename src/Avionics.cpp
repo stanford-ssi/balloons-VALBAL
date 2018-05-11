49c7b243accc9aee288cc983e938ad8b4fcc5008
Joan Creus-Costa <jcreus@stanford.edu>
2018-05-11 04:31:37 +0000

    fixing sign of motor current

--- src/Avionics.cpp
+++ src/Avionics.cpp
@@ -329,7 +329,7 @@ bool Avionics::readData() {
   data.JOULES_TOTAL               += data.LOOP_TIME/1000.*data.VOLTAGE_PRIMARY*data.CURRENT_TOTAL/1000.;
   data.CURRENT_RB                 = -currentSensor.average_voltage_readings(DIFF_14_15, CURRENT_NUM_SAMPLES);
   data.MAX_CURRENT_CHARGING_LIMIT = superCap.getChargingLimit();
-  float motcur = -currentSensor.average_voltage_readings(DIFF_8_9, CURRENT_NUM_SAMPLES);
+  float motcur = currentSensor.average_voltage_readings(DIFF_8_9, CURRENT_NUM_SAMPLES);
   data.CURRENT_MOTOR_VALVE        = (data.VALVE_STATE ? motcur : 0);
   data.CURRENT_MOTOR_BALLAST      = (data.BALLAST_STATE ? motcur : 0);
   data.CURRENT_MOTORS             = motcur;
@@ -797,7 +797,7 @@ bool Avionics::runLED() {
  * This function interfaces with the payload.
  */
 bool Avionics::runRadio() {
-  if (!data.POWER_STATE_PAYLOAD) return true;
+  if (!data.POWER_STATE_RADIO) return true;
   radio.readyDataFrame();
   radio.addVariable(data.ALTITUDE_BAROMETER, -100, 25000, 16);
   radio.addVariable(data.ASCENT_RATE, -6, 6, 10);

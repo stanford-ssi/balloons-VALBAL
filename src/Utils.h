24655ab666928079a71aba6edb453ed2f288388a
John Dean <deanjl@stanford.edu>
2018-08-20 20:45:28 -0700

    long comment about how to use a lowpass filter

--- src/Utils.h
+++ src/Utils.h
@@ -87,6 +87,28 @@ private:
  * class: AdjustableLowpass
  * -------------------
  * Adjustable 2nd order IIR lowpass filter. Wrapper for biquad
+ * 
+ * F0: corner frequency of the filter
+ * Q: Quality factor of the filter
+ * Fs: Sample rate of the filter
+ * 
+ * If you don't know how what you are doing, but just want a lowpass filter, then
+ * do the following: 
+ * Call the constructor and give it the three settings. For general use, you don't need to 
+ * know what Q means, and just set it to 0.5. F0 is the corner frequency, which can be thought
+ * of as the freuqency at which waves of higher frequency will be rejected by the filter, and 
+ * waves of lower freuqecny will be passed through by the filter. Sometimes it's easy to pick
+ * this value exactly, but other times it's not and it's best to just run the filter on previous
+ * data and tune the corner frequency until it "looks right". A lower corner frequency means more
+ * smoothing, but also more delay between the outputs and the inputs. The corner frequency must be
+ * less than half of the sample rate, and in most cases it will be much, much less. 
+ * The sample rate is the freuency at which you call the update function.
+ * To get the filtered value from the filter, simply call update(input), where input is the raw
+ * value that you are looking to filter. It will return the filtered value for that timestep.
+ * You need to ensure that you call update() at a fixed frequency, best done with an interrupt timer.
+ *
+ * Lastly, these things are not foolproof so some spooky stuff can happen if you do things wrong. 
+ * Probably a good idea to check the final implementation and use with @johndean
  */
 class AdjustableLowpass{
 public:

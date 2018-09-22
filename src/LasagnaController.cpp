8823e55895022beaee2c3b35bc5425fb6c7c113f
John Dean <deanjl@stanford.edu>
2018-09-22 03:02:08 -0400

    wow I actually added comments to lasagna

--- src/LasagnaController.cpp
+++ src/LasagnaController.cpp
@@ -33,6 +33,7 @@ bool LasagnaController::update(Input input){
     state.v1 = v1_filter.update((input.h_rel-state.h_rel_last)*constants.freq);
     state.v2 = v2_filter.update((input.h_rel-state.h_rel_last)*constants.freq);
   }
+
   /**
    * Cases and transistions for various flight states
    */
@@ -54,9 +55,17 @@ bool LasagnaController::update(Input input){
     case EQUIL:
       break;
   }
-  state.v_dldt = input.op*constants.v_dldt_a + constants.v_dldt_b; 
-  state.v = (state.status==EQUIL) ? state.v1 : state.v2;
 
+  state.v_dldt = input.op*constants.v_dldt_a + constants.v_dldt_b; 
+  state.v = (state.status==EQUIL) ? state.v1 : state.v2; // v1 is low corner freqency, so it's used after equilibration
+
+  /**
+   * Predict changes in velocity from external dldt and dldt from actions. This is only done after the
+   * balloon has equilibrated. We then sum all these delta v's (state.dv_sum). We add this sum to the 
+   * fused_v, however we also substract the dv_sum passed through the action_filter. This is becuase the 
+   * action filter has the same constants of the velocity filter, so it reflects the component of the predicted
+   * dv_sum that should already be observed in the filtered velocity.
+   */
   float dv_pred = 0;
   if(state.status==EQUIL){
     float act_dldt = state.action > 0 ? float(state.action)*constants.b_dldt : float(state.action)*state.v_dldt*constants.kfuse_val;
@@ -64,8 +73,13 @@ bool LasagnaController::update(Input input){
   }
   state.dv_sum += dv_pred;  
   state.fused_v = state.v + state.dv_sum - action_filter.update(state.dv_sum);
+
+  /**
+   * Now the inner loop is called which computes the actions given the estimated velocity and altitude
+   */
   innerLoop(input.h_abs);
-  if(state.status==PRELAUNCH) state.action = 0;
+
+  if(state.status==PRELAUNCH) state.action = 0;  // if we haven't launched yet, don't do any actions
 
   state.h_rel_last = input.h_rel;
   return true;
@@ -73,6 +87,10 @@ bool LasagnaController::update(Input input){
 
 
 void LasagnaController::innerLoop(float input_h){
+
+  /**
+   * compute effort pre-deadband
+   */
   if(state.comp_ctr >= comp_freq*constants.freq){
     state.v_cmd = constants.k_h * (constants.h_cmd - input_h);
     state.v_cmd_clamped = pasta_clamp(state.v_cmd,-(constants.k_h*constants.ss_error_thresh+constants.v_limit),(constants.k_h*constants.ss_error_thresh+constants.v_limit));
@@ -83,15 +101,23 @@ void LasagnaController::innerLoop(float input_h){
     state.comp_ctr = 0;
   }
 
+  /**
+   * deadband
+   */
   float deadband_effort = 0;
   float thresh = constants.k_v*constants.k_h*constants.ss_error_thresh;
-  //float thresh = 0;
   if(jankabs(state.effort)-thresh > 0){
     deadband_effort = state.effort + ((state.effort<0)-(state.effort>0))*thresh;
   }
   deadband_effort = pasta_clamp(deadband_effort,-state.v_dldt,constants.b_dldt);
-  //if(state.v>constants.v_limit) deadband_effort = -max(-deadband_effort,float(0));
-  //if(state.v<-constants.v_limit) deadband_effort = max(deadband_effort,float(0));
+
+  /**
+   * keeps track of a sum of the total desired effort (which is in kg/s since its a dldt)
+   * from the controller. Once that effort is greater than the minimum vent or ballast ammout,
+   * then an action happens. It could also be mutliples of the minimum ammouts, in which case multple
+   * vent would happen. At 20Hz, there would never be multiple events in one control cycle, but at very low 
+   * frequencies, it could happen.
+   */
   state.effort_sum += deadband_effort/constants.freq;
   if(state.effort_sum >= constants.b_tmin*constants.b_dldt){
     state.action = constants.b_tmin*int(state.effort_sum/(constants.b_tmin*constants.b_dldt));
